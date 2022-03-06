ecb4b16471acbbd43c63db3a3d67dc8a72f98ece
Trần Trung Tín <trungtindev@gmail.com>
2022-03-07 00:22:56 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import Banner from '../images/banner.jpg'
+
+import Map from '../images/map.png'
+import About from '../images/about.jpg'
+
+import P1 from '../images/p1.jpg'
+import P2 from '../images/p2.jpg'
+import P3 from '../images/p3.jpg'
+import P4 from '../images/p4.jpg'
+import P5 from '../images/p5.jpg'
+import P6 from '../images/p6.jpg'
+
+import Clock from '../images/icons/Clock.svg'
+import Pin from '../images/icons/Pin.svg'
+import Micro from '../images/icons/Micro.svg'
+
+const linkClass = ''
+const lightTextClass = 'text-gray-lightText text-base font-light'
 
 const Home: NextPage = () => {
+  const renderPerson = (person: any, index: number) => (
+    <div className="flex-1" key={index}>
+      <div>
+        <Image src={person.i} alt={person.n}></Image>
+      </div>
+      <h3 className="h3 mt-6">{person.n}</h3>
+      <p className={`${lightTextClass} mt-2`}>{person.r}</p>
+    </div>
+  )
   return (
     <div className="">
       <Head>
@@ -11,25 +38,46 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="min-h-screen flex-1 flex flex-col">
-        <article className="container">
-          <h1 className="h1">About</h1>
-          <div>
-            <p className="font-semibold text-xl leading-relaxed">
-              Our aim is to host Ethereum Magicians Council, similar as what our
-              OG Community is used to - fish bowl / circle discussions, topic
-              voting, group discussions. We are going to host a few session
-              leaders that will help us lead sessions and encourage people to
-              talk about the issues we are facing in the Ethereum Ecosystem.
-            </p>
+        <article
+          className="w-full relative"
+        >
+          <Image src={Banner} alt="" layout="responsive"></Image>
+        </article>
+        <article className="container pt-32">
+          <h1 className="h1 px-24 mb-14">About</h1>
+          <div className="rounded-5xl bg-gray-bg pl-24 grid grid-cols-10 gap-8 mr-24">
+            <div className="py-12 col-span-6">
+              <p className="">
+                Our aim is to host Ethereum Magicians Council, similar as what
+                our OG Community is used to - fish bowl / circle discussions,
+                topic voting, group discussions.
+              </p>
+              <p className="mt-4">
+                We are going to host a few session leaders that will help us
+                lead sessions and encourage people to talk about the issues we
+                are facing in the Ethereum Ecosystem.
+              </p>
+            </div>
+            <div className="col-start-7 col-span-4 relative">
+              <div className="absolute w-full -top-20 bottom-6 -right-24">
+                <Image
+                  src={About}
+                  alt=""
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-4xl"
+                ></Image>
+              </div>
+            </div>
           </div>
 
-          <div className="font-light text-base leading-loose text-gray-03">
+          <div className={`${lightTextClass} text-gray-03 py-12 px-24`}>
             <p>
               Council is a big gathering of Ethereum enthusiasts with aim to
               discuss and solve issues we are facing in the Ethereum core
               ecosystem.
             </p>
-            <p>
+            <p className="mt-3">
               We are going to host 250 people over the period of two days with
               the aim to provide place for people to discuss issues on Ethereum.
               We are going to do Council with voting process at the venue
@@ -37,33 +85,73 @@ const Home: NextPage = () => {
           </div>
         </article>
 
-        <article className="container">
+        <article className="container mt-48 text-center">
           <h1 className="h1 text-center">Sessions & Session Leaders</h1>
-          <div>
-            <h3>Day 1 - Saturday April 23rd</h3>
-            <p>
+          <div className="mt-24">
+            <div className="">
+              <Image src={Micro} alt=""></Image>
+            </div>
+            <h3 className="h3 mb-4 mt-10">Day 1 - Saturday April 23rd</h3>
+            <p className="text-gray-subText">
               Saturday will be focused on dApp layer EIPs and topic
               disccussions. NFT, DeFi related discussions are welcomed, we would
               like to look into standardisation of DeFi and NFT protocols.
             </p>
+            <div className="grid grid-cols-3 gap-7 mt-10">
+              {[
+                { i: P1, n: 'James Hacock', r: 'Ex AllCoreDevs Coordinator ' },
+                {
+                  i: P2,
+                  n: 'Anett',
+                  r: 'NFT Standards - Author of NFTStandards.wtf documentation, Working Group leader ',
+                },
+                {
+                  i: P3,
+                  n: 'Hadrien',
+                  r: 'ERC-4626 - Author of ERC-4626 Open Zeppelin implementation',
+                },
+              ].map(renderPerson)}
+            </div>
           </div>
-          <div>
-            <h3>Day 2 - Sunday April 24th</h3>
-            <p>
+          <div className="mt-24">
+            <div className="">
+              <Image src={Micro} alt=""></Image>
+            </div>
+            <h3 className="h3 mb-4 mt-10">Day 2 - Sunday April 24th</h3>
+            <p className="text-gray-subText">
               Sunday will be focused on protocol layer EIPs and discussions.
               This day be can be used for community discussions with Core Devs
               and protocol researchers
             </p>
+            <div className="grid grid-cols-3 gap-7 mt-10">
+              {[
+                {
+                  i: P4,
+                  n: 'Piper Merriam',
+                  r: 'Portal Network (ex Stateless Client)',
+                },
+                {
+                  i: P5,
+                  n: 'ETH Staker Team',
+                  r: 'Client UX',
+                },
+                {
+                  i: P6,
+                  n: 'Marius Van Der Wijden',
+                  r: 'Merge Testing',
+                },
+              ].map(renderPerson)}
+            </div>
           </div>
-          <p>
+          <p className={`${lightTextClass} mt-12`}>
             Both days the sessions will start at 10am with Opening Ceremony.
           </p>
         </article>
 
-        <article className="container py-12 mt-12">
+        <article className="container py-12 mt-48">
           <h1 className="h1 mb-8">Tickets</h1>
           <div className="grid grid-cols-8 gap-x-8">
-            <div className="col-span-5 leading-relaxed">
+            <div className="col-span-5">
               <p className="mb-4">
                 We are going to give away tickets for FREE with optional
                 donation. Tickets will be available in 3 waves. The first wave
@@ -85,13 +173,17 @@ const Home: NextPage = () => {
                   protocol database (All Core Devs)
                 </li>
               </ul>
-              <p className="text-gray-lightText text-base font-light mt-6">
-                The second and third wave tickets will be publicly available to
-                Ethereum Community (everyone) on our OG Council Website.All
-                tickets will have the same value & permissions to the event
-                First two ticket waves will have 7 days time period to claim
-                their ticket
-              </p>
+              <div className={`${lightTextClass} mt-6`}>
+                <p>
+                  The second and third wave tickets will be publicly available
+                  to Ethereum Community (everyone) on our OG Council Website.All
+                  tickets will have the same value & permissions to the event
+                </p>
+                <p>
+                  First two ticket waves will have 7 days time period to claim
+                  their ticket
+                </p>
+              </div>
             </div>
             <div className="col-span-3"></div>
           </div>
@@ -114,7 +206,7 @@ const Home: NextPage = () => {
 
         <article className="container mt-12 py-12">
           <h1 className="h1">Agenda</h1>
-          <p className="text-gray-lightText text-base font-light mt-12">
+          <p className={`${lightTextClass} mt-12`}>
             Agenda will be very similar for both days
           </p>
           <div className="rounded-2xl bg-gray-bg px-6 py-2 mt-6">
@@ -155,55 +247,65 @@ const Home: NextPage = () => {
             wider audience Optionally groups can regroup again and discuss &
             work together again
           </p>
-          <p className="text-gray-lightText text-base font-light mt-4">
+          <p className={`${lightTextClass} mt-4`}>
             The venue has no closing time so we can use it as long as we need -
             if session leaders would like to work on propoosal after 6pm
           </p>
         </article>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <article className="container mb-36 py-12">
+          <h1 className="h1 text-center mb-22">Time & Place</h1>
+          <div className="grid grid-cols-10 gap-9">
+            <div className="col-span-4">
+              <div className="">
+                <Image
+                  src={Map}
+                  objectFit="contain"
+                  alt="Map"
+                  className="rounded-3xl"
+                ></Image>
+              </div>
+            </div>
+            <div className="col-span-6">
+              <div className="flex">
+                <div className="mr-5 flex-shrink-0">
+                  <Image src={Clock} alt=""></Image>
+                </div>
+                <div>
+                  <h3 className="h3 mb-2">Time</h3>
+                  <p>23 & 24 April (Saturday & Sunday)</p>
+                </div>
+              </div>
+              <div className="mt-9 flex">
+                <div className="mr-5 flex-shrink-0">
+                  <Image src={Pin} alt=""></Image>
+                </div>
+                <div>
+                  <h3 className="h3 mb-2">Place</h3>
+                  <p>
+                    23 & 24 April (SaPakhuis de Zwijger is a famous old
+                    warehouse and located at ‘IJ’, the main river crossing
+                    Amsterdam. Public transport is in front of the venue and
+                    it’s a 26 minutes walk and 7 minutes bike ride away from
+                    Devconnect’s co-working space in Beurs van Berlage.turday &
+                    Sunday)
+                  </p>
+                  <div className="mt-6">
+                    <a
+                      href="https://maps.google.com"
+                      className="inline-block bg-gray-link cursor-pointer hover:bg-gray-linkHover uppercase px-5 py-3 rounded-full"
+                    >
+                      Directions
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer></footer>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     colors: {
       transparent: 'transparent',
@@ -9,8 +10,11 @@ module.exports = {
       black: '#000',
       gray: {
         lightText: '#858585',
+        subText: '#8C8C8C',
         bg: '#F4F4F4',
         hr: '#E0E0E0',
+        link: '#D9D9D9',
+        linkHover: '#CFCFCF',
       },
     },
     container: {
@@ -20,20 +24,24 @@ module.exports = {
         sm: '2rem',
         lg: '4rem',
         xl: '5rem',
-        '2xl': '6rem',
+        '2xl': '13rem',
       },
     },
     extend: {
       fontFamily: {
         body: `"Comfortaa", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
       },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['active'],
-      borderColor: ['active'],
-      cursor: ['responsive', 'hover'],
+      fontSize: {
+        base: ['16px', '30px'],
+        xl: ['20px', '37px']
+      },
+      spacing: {
+        22: '5.5rem',
+      },
+      borderRadius: {
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+      },
     },
   },
   plugins: [],
